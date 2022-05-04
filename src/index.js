323c65d47796c52858150f4568068578ce77f4bd
Esteban Frare <esteban.frare@radiumrocket.com>
2022-05-04 13:26:07 -0300

    BaSP: setup folder structure

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
+// use "import" to import libraries
 import express from 'express';
+
+// use "require" to import JSON files
+const admins = require('./data/admins.json');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/admins', (req, res) => {
+  res.status(200).json({
+    data: admins,
+  });
 });
 
 app.listen(port, () => {
