2ce68fb7082c7e43b29f1790a3ddd734b07281e3
Esteban Frare <esteban.frare@radiumrocket.com>
2022-05-19 08:12:20 -0300

    BaSP: fix MongoMemoryServer setup

--- jest.setup.js
+++ jest.setup.js
@@ -2,10 +2,11 @@ import mongoose from 'mongoose';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 
 const mongoServer = new MongoMemoryServer();
+let mongo;
 
 // Make the connection to a fake database
 beforeAll(async () => {
-  const mongo = await MongoMemoryServer.create();
+  mongo = await MongoMemoryServer.create();
   const uri = mongo.getUri();
 
   const mongooseOpts = {
@@ -20,4 +21,5 @@ afterAll(async () => {
   await mongoose.connection.dropDatabase();
   await mongoose.connection.close();
   await mongoServer.stop();
+  await mongo.stop();
 });
